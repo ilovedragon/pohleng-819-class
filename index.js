831a918095be5723a5f46e7a50dc8cc70fb5eaba
ilovedragon <ngpohleng@yahoo.com>
2023-08-19 12:40:30 +0800

    initial commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ module.exports.handler = async (event) => {
       body: JSON.stringify(
         {
           message: "Your function executed successfully!",
+          access_local_key: process.env.access_local_key
+          access_key: process.env.access_key
         },
         null,
         2
