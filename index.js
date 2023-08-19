5c059e2800472d75875d7b8de74f9ab02f8c2810
ilovedragon <ngpohleng@yahoo.com>
2023-08-19 18:15:54 +0800

    initial commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.handler = async (event) => {
         {
           message: "Your function executed successfully!",
           access_local_key: process.env.access_local_key,
-          access_key: process.env.ACCESS_KEY
+          access_key: process.env.ACCESS_KEY,
           poh_access_key: process.env.POH_ACCESS_KEY
         },
         null,
